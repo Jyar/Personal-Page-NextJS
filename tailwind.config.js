c6d4db2194e10301a9c7c6097d6a6191593078cf
Jyar <jyar27@yahoo.com>
2023-06-19 17:23:24 -0400

    remove theme provider

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,8 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite-react/**/*.js",
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [require("flowbite/plugin")],
 };
