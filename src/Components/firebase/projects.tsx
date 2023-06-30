f43bb0299e085b8eb8ead21340b22c4856c6a435
Jyar <jyar27@yahoo.com>
2023-06-30 01:40:41 -0400

    wait on firebase init

--- src/Components/firebase/projects.tsx
+++ src/Components/firebase/projects.tsx
@@ -1,8 +1,9 @@
 import { ref, set } from "firebase/database";
-import { db } from "../../../utils/firebase";
+import { initDB } from "../../../utils/firebase";
 import { FormData } from "../types/FormData";
 
 function writeUserData(data: FormData) {
+  let db = initDB();
   if (db) {
     set(ref(db, "users/" + (data.first + data.last).toLowerCase()), data);
   }
--- utils/firebase.tsx
+++ utils/firebase.tsx
@@ -5,24 +5,26 @@ import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-let firebaseConfig = {
-  apiKey: String(process.env.APIKEY) || "",
-  authDomain: String(process.env.AUTHDOMAIN) || "",
-  projectId: String(process.env.PROJECTID) || "",
-  storageBucket: String(process.env.STORAGEBUCKET) || "",
-  messagingSenderId: String(process.env.MESSAGINGSENDERID) || "",
-  appId: String(process.env.APPID) || "",
-  databaseURL: String(process.env.DATABASEURL) || "",
-  measurementId: String(process.env.MEASUREMENTID) || "",
-};
+export function initDB() {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  let firebaseConfig = {
+    apiKey: String(process.env.APIKEY) || "",
+    authDomain: String(process.env.AUTHDOMAIN) || "",
+    projectId: String(process.env.PROJECTID) || "",
+    storageBucket: String(process.env.STORAGEBUCKET) || "",
+    messagingSenderId: String(process.env.MESSAGINGSENDERID) || "",
+    appId: String(process.env.APPID) || "",
+    databaseURL: String(process.env.DATABASEURL) || "",
+    measurementId: String(process.env.MEASUREMENTID) || "",
+  };
 
-let app = null;
-let getDB = null;
-// Initialize Firebase
-if (!firebase.apps.length) {
-  app = initializeApp(firebaseConfig);
-  getDB = getDatabase(app);
+  let app = null;
+  let getDB = null;
+  // Initialize Firebase
+  if (!firebase.apps.length) {
+    app = initializeApp(firebaseConfig);
+    getDB = getDatabase(app);
+  }
+  return getDB;
 }
-export const db = getDB;
