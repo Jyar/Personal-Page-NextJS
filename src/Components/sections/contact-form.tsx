9115de6475bbf176ded5648cf9a3bc551789af39
Jyar <jyar27@yahoo.com>
2023-07-20 21:38:34 -0400

    change text

--- src/Components/sections/contact-form.tsx
+++ src/Components/sections/contact-form.tsx
@@ -125,7 +125,7 @@ export const ContactForm = () => {
                 htmlFor="floating_company"
                 className="text-sm sm:text-lg peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Company (Ex. Google) or Casual Visitor
+                Company (Ex. Google) or Visitor
               </label>
             </div>
           </div>
