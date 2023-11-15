18ef2405d01aca910f886588922580f75e1f16ee
Jyar <jyar27@yahoo.com>
2023-11-14 20:03:21 -0500

    remove aos

--- src/Components/content.tsx
+++ src/Components/content.tsx
@@ -22,59 +22,59 @@ import IsotopeTable from "./items/isotope-table";
 
 export const Content = () => {
   let strokeColor = "blue";
-  useLayoutEffect(() => {
-    AOS.init({
-      easing: "ease-out-cubic",
-      offset: 50,
-    });
+  // useLayoutEffect(() => {
+  //   AOS.init({
+  //     easing: "ease-out-cubic",
+  //     offset: 50,
+  //   });
 
-    // let ctx = gsap.context(() => {
-    //   gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
-    //   gsap.defaults({ ease: "none" });
+  // let ctx = gsap.context(() => {
+  //   gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
+  //   gsap.defaults({ ease: "none" });
 
-    //   const pulses = gsap
-    //     .timeline({
-    //       defaults: {
-    //         duration: 0.05,
-    //         autoAlpha: 1,
-    //         scale: 2,
-    //         transformOrigin: "center",
-    //         ease: "elastic(2.5, 1)",
-    //       },
-    //     })
-    //     .to(".ball02, .text01", {}, 0.2)
-    //     .to(".ball03, .text02", {}, 0.33)
-    //     .to(".ball04, .text03", {}, 0.46);
+  //   const pulses = gsap
+  //     .timeline({
+  //       defaults: {
+  //         duration: 0.05,
+  //         autoAlpha: 1,
+  //         scale: 2,
+  //         transformOrigin: "center",
+  //         ease: "elastic(2.5, 1)",
+  //       },
+  //     })
+  //     .to(".ball02, .text01", {}, 0.2)
+  //     .to(".ball03, .text02", {}, 0.33)
+  //     .to(".ball04, .text03", {}, 0.46);
 
-    //   const main = gsap
-    //     .timeline({
-    //       defaults: { duration: 1 },
-    //       scrollTrigger: {
-    //         trigger: "#svg",
-    //         scrub: true,
-    //         start: "top center",
-    //         end: "bottom center",
-    //       },
-    //     })
-    //     .to(".ball01", { duration: 0.01, autoAlpha: 1 })
-    //     .from(".theLine", { drawSVG: 0 }, 0)
-    //     .to(
-    //       ".ball01",
-    //       {
-    //         motionPath: {
-    //           path: ".theLine",
-    //           align: ".theLine",
-    //           alignOrigin: [0.5, 0.5],
-    //         },
-    //       },
-    //       0
-    //     )
-    //     .add(pulses, 0);
-    // }
-    // );
+  //   const main = gsap
+  //     .timeline({
+  //       defaults: { duration: 1 },
+  //       scrollTrigger: {
+  //         trigger: "#svg",
+  //         scrub: true,
+  //         start: "top center",
+  //         end: "bottom center",
+  //       },
+  //     })
+  //     .to(".ball01", { duration: 0.01, autoAlpha: 1 })
+  //     .from(".theLine", { drawSVG: 0 }, 0)
+  //     .to(
+  //       ".ball01",
+  //       {
+  //         motionPath: {
+  //           path: ".theLine",
+  //           align: ".theLine",
+  //           alignOrigin: [0.5, 0.5],
+  //         },
+  //       },
+  //       0
+  //     )
+  //     .add(pulses, 0);
+  // }
+  // );
 
-    // return () => ctx.revert(); // <-- CLEANUP!
-  }, []);
+  // return () => ctx.revert(); // <-- CLEANUP!
+  // }, []);
 
   return (
     <>
